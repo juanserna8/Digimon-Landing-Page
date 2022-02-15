d084608b73eb00694ed6ab8f4e5b7aca71838409
Juan David Serna <sernadominguezj@gmail.com>
2022-02-15 13:31:06 +1100

    The media query points have been modified. Additionally, we specified that the layout will only be shown in large screens

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    screens: {
+      'sm': '0px',
+      'md': '768px',
+      'lg': '1024px'
+    },
     extend: {},
   },
   plugins: [],
