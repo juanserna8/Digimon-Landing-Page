cf672a0dfaa8ede0e6d609187ac7608a49f60cee
Juan David Serna <sernadominguezj@gmail.com>
2022-03-06 10:43:26 +1100

    Hovers and interactivity

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,7 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    screens: {
-      'sm': '0px',
-      'md': '768px',
-      'lg': '1024px'
-    },
+    
     extend: {},
   },
   plugins: [],
